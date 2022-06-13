db2062ce0ca1f8563a08ca8df857c4b874b10ab5
T-otor <vuettwiller@ecowan.fr>
2022-06-13 11:41:26 +0200

    J'ai envie de faire du css + réparer le crud

--- webapp/models.py
+++ webapp/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class categorie(models.Model):
+    Nom = models.CharField(max_length=100)
+    Descriptif = models.CharField(max_length=10000)
+
+    def __str__(self):
+        chaine = f"Nom {self.Nom}, Descriptif {self.Descriptif}"
+        return chaine
--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-#from .forms import FormCategorie
+from .forms import FormCategorie
 
 # Create your views here.
 
 # Ajouter 
 def index(request):
     return render(request, "webapp/index.html")
+
+def ajout(request):
+    if request.method == "POST":
+        form = FormCategorie(request)
+        if form.is_valid():
+            return HttpResponseRedirect("webapp/traitement_categorie")
+        else:
+            return render(request, 'webapp/ajout.html', {'form': form})
+    else:
+        form = FormCategorie()
+        return render(request, 'webapp/ajout.html', {"form": form})
+
+def traitement_categorie(request):
+    lform = FormCategorie(request.POST)
+    if lform.is_valid():
+        categorie = lform.save()
+        return render(request, 'webapp/index',{"Categorie", categorie})
+    else:
+        return render(request, 'webapp/ajout.html',{"form", lform})
