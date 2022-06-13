d6650a501882edcd8b8c99f7cd03974b17d59c88
T-otor <vuettwiller@ecowan.fr>
2022-06-13 13:04:53 +0200

    Remise à zéro COMPLETE du django suite à des corruptions

--- webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from operator import index
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('index/', views.index),
-    path('ajout_categorie/', views.ajout),
-    path('traitement_categorie/', views.traitement_categorie),
-]
